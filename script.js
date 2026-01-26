dce31576333654ef759434fa6f973463f290fc40
adrijus656 <127625370+adrijus656@users.noreply.github.com>
2026-01-26 21:07:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function yesClick() {
+  document.body.style.backgroundImage =
+    "url('https://media.tenor.com/0jDvy_gaK7EAAAAC/milk-bear-sending-hearts.gif')";
+  document.getElementById("text").innerText = "YAYYYY!!!! 💖🥰";
+}
+
+function noClick() {
+  document.body.style.backgroundImage =
+    "url('https://media1.tenor.com/m/RJgIui1E_2QAAAAC/teddy-bear.gif')";
+  document.getElementById("text").innerText = "awww 🥲";
+}
