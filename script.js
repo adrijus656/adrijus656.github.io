4fdef4ca5514681a7a03cebda11dc41154cf6084
adrijus656 <127625370+adrijus656@users.noreply.github.com>
2026-01-26 21:18:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+const webhookURL = "https://discord.com/api/webhooks/1465424477595631616/GAUugpZmy3QwDHEkhCG3oaHwil6Rwh5PpzJ2ictm_3GBw_SwFwBuLEV2L2RMbpJINSFn";
+
+function sendToDiscord(message) {
+  fetch(webhookURL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ content: message })
+  });
+}
+
 function yesClick() {
   document.body.style.backgroundImage =
-    "url('https://media.tenor.com/0jDvy_gaK7EAAAAC/milk-bear-sending-hearts.gif')";
-  document.getElementById("text").innerText = "YAYYYY!!!! 💖🥰";
+    "url('https://media0.giphy.com/media/JRsQiAN79bPWUv43Ko/giphy.gif?cid=6c09b95255n79kekmfiq6n2kat80vtezoilup8azapeb9t8v&ep=v1_gifs_search&rid=giphy.gif&ct=s')";
+  
+  const text = document.getElementById("text");
+  text.innerText = "YAYYYY!!!! 💖🥰";
+  text.classList.remove("fade-in");
+  void text.offsetWidth;
+  text.classList.add("fade-in");
+
+  // SEND LOG TO DISCORD
+  sendToDiscord("Someone pressed YES! 💕");
 }
 
 function noClick() {
   document.body.style.backgroundImage =
     "url('https://media1.tenor.com/m/RJgIui1E_2QAAAAC/teddy-bear.gif')";
-  document.getElementById("text").innerText = "awww 🥲";
+  
+  const text = document.getElementById("text");
+  text.innerText = "awww 🥲";
+  text.classList.remove("fade-in");
+  void text.offsetWidth;
+  text.classList.add("fade-in");
+
+  // SEND LOG TO DISCORD
+  sendToDiscord("Someone pressed NO 😭");
 }
